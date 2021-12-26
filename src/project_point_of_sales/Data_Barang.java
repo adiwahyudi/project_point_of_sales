42198ec3d6463414ecd7edcdffadc26b5d52f1ab
fahrul31 <fahrulhidayat73@gmail.com>
2021-12-27 04:40:46 +0800

    Update Data_Barang.java

--- src/project_point_of_sales/Data_Barang.java
+++ src/project_point_of_sales/Data_Barang.java
@@ -1,14 +1,20 @@
 
 package project_point_of_sales;
 
+import java.sql.*;
+
 
 public class Data_Barang {
-    private String id_Barang,nama_Barang,kategori;
+//    private int id_Barang=1;
+    private String nama_Barang,kategori;
     private int harga_jual,harga_beli;
+    private boolean pesan = false;
+    koneksi kn = new koneksi();
+    Connection kon = kn.getKoneksi();
 
-    public String getId_Barang() {
-        return id_Barang;
-    }
+//    public int getId_Barang() {
+//        return id_Barang;
+//    }
 
     public String getNama_Barang() {
         return nama_Barang;
@@ -26,9 +32,6 @@ public class Data_Barang {
         return harga_beli;
     }
 
-    public void setId_Barang(String id_Barang) {
-        this.id_Barang = id_Barang;
-    }
 
     public void setNama_Barang(String nama_Barang) {
         this.nama_Barang = nama_Barang;
@@ -45,6 +48,28 @@ public class Data_Barang {
     public void setHarga_beli(int harga_beli) {
         this.harga_beli = harga_beli;
     }
+
+    public boolean isPesan() {
+        return pesan;
+    }
     
-    public void displayDataBarang(){}
+    
+    public void simpan(){
+        try{
+            Statement stmt = (Statement) kn.getKoneksi().createStatement();
+            String sql = "INSERT INTO data_barang(nama_Barang,kategori,harga_jual,harga_beli) VALUES ('"+this.nama_Barang+"','"+this.kategori+"',"+this.harga_jual+","+this.harga_beli+")";
+            stmt.executeUpdate(sql);
+            this.pesan = true;
+        }catch(SQLException sqle){
+            System.out.println(sqle);
+        }
+    }
+    
+    public void displayDataBarang(){
+//        System.out.println("ID Barang           :" + this.id_Barang);
+        System.out.println("Nama Barang         :" + this.nama_Barang);
+        System.out.println("Kategori            :" + this.kategori);
+        System.out.println("Harga Jual          :" + this.harga_jual);
+        System.out.println("Harga Beli          :" + this.harga_beli);
+    }
 }
