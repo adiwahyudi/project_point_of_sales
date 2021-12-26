035cb29c9cae4b0db31893d76a53231ff7af5ed2
fahrul31 <fahrulhidayat73@gmail.com>
2021-12-27 07:35:48 +0800

    Update

--- src/GUI/Frame_DetailBelanja.java
+++ src/GUI/Frame_DetailBelanja.java
@@ -5,6 +5,8 @@
  */
 package GUI;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author ASUS
@@ -19,6 +21,7 @@ public class Frame_DetailBelanja extends javax.swing.JFrame {
     public Frame_DetailBelanja() {
         initComponents();
         dis.Display_dataBarang(tab_dataBarang);
+        dis.Display_detailBelanja(tab_detailBelanja);
     }
 
     /**
@@ -33,6 +36,14 @@ public class Frame_DetailBelanja extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tab_dataBarang = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        add_keranjang = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tab_detailBelanja = new javax.swing.JTable();
+        inp_id = new javax.swing.JTextField();
+        inp_namaBarang = new javax.swing.JTextField();
+        inp_hargaJual = new javax.swing.JTextField();
+        inp_kategori = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,38 +52,118 @@ public class Frame_DetailBelanja extends javax.swing.JFrame {
 
             },
             new String [] {
-                "id", "nama_Produk", "harga_Jual"
+                "id", "nama_Produk", "kategori", "harga_Jual"
             }
         ));
+        tab_dataBarang.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tab_dataBarangMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tab_dataBarang);
 
         jLabel1.setText("Daftar Produk");
+
+        jLabel2.setText("Keranjang");
+
+        add_keranjang.setText("Tambah ke keranjang");
+        add_keranjang.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                add_keranjangActionPerformed(evt);
+            }
+        });
+
+        tab_detailBelanja.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "id", "nama_Produk", "kategori", "harga_Jual"
+            }
+        ));
+        jScrollPane2.setViewportView(tab_detailBelanja);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 457, Short.MAX_VALUE)
+                .addContainerGap(63, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(inp_id, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(inp_hargaJual, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(inp_namaBarang, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(inp_kategori, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(195, 195, 195)
+                            .addComponent(jLabel1))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 444, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(add_keranjang)
+                        .addGap(9, 9, 9)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(203, 203, 203)
+                                .addComponent(jLabel2))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 444, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(205, 205, 205)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(19, 19, 19)
+                .addGap(24, 24, 24)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(165, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(30, 30, 30)
+                                .addComponent(jLabel2))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(13, 13, 13)
+                                .addComponent(add_keranjang)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(94, 94, 94)
+                        .addComponent(inp_id, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(13, 13, 13)
+                        .addComponent(inp_namaBarang, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(9, 9, 9)
+                        .addComponent(inp_kategori, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(inp_hargaJual, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void add_keranjangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_add_keranjangActionPerformed
+        // TODO add your handling code here:
+        dis.add_Keranjang(inp_id.getText(), inp_namaBarang.getText(), inp_kategori.getText(),inp_hargaJual.getText());
+        dis.Display_detailBelanja(tab_detailBelanja);
+    }//GEN-LAST:event_add_keranjangActionPerformed
+
+    private void tab_dataBarangMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tab_dataBarangMouseClicked
+        // TODO add your handling code here:
+        DefaultTableModel tblModel = (DefaultTableModel) tab_dataBarang.getModel();
+        
+        String tbl_ID = (String) tblModel.getValueAt(tab_dataBarang.getSelectedRow(),0);
+        String tbl_namaProduk = (String) tblModel.getValueAt(tab_dataBarang.getSelectedRow(),1);
+        String tbl_kategori = (String) tblModel.getValueAt(tab_dataBarang.getSelectedRow(),2);
+        String tbl_hargaJual = (String) tblModel.getValueAt(tab_dataBarang.getSelectedRow(),3);
+        
+        inp_id.setText(tbl_ID);
+        inp_namaBarang.setText(tbl_namaProduk);
+        inp_kategori.setText(tbl_kategori);
+        inp_hargaJual.setText(tbl_hargaJual);
+    }//GEN-LAST:event_tab_dataBarangMouseClicked
 
     /**
      * @param args the command line arguments
@@ -110,8 +201,16 @@ public class Frame_DetailBelanja extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton add_keranjang;
+    private javax.swing.JTextField inp_hargaJual;
+    private javax.swing.JTextField inp_id;
+    private javax.swing.JTextField inp_kategori;
+    private javax.swing.JTextField inp_namaBarang;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable tab_dataBarang;
+    private javax.swing.JTable tab_detailBelanja;
     // End of variables declaration//GEN-END:variables
 }
--- src/GUI/display.java
+++ src/GUI/display.java
@@ -21,19 +21,59 @@ public class display {
     public void Display_dataBarang(JTable jtable){
         
         try{
+            DefaultTableModel tblModel = (DefaultTableModel) jtable.getModel();
+            tblModel.setRowCount(0);
             Statement stmt = (Statement) kn.getKoneksi().createStatement();
             String sql = "Select * FROM data_barang";
             ResultSet rs = stmt.executeQuery(sql);
             while(rs.next()){
                 String id = String.valueOf(rs.getInt("id_barang"));
                 String nama_Produk = rs.getString("nama_Barang");
+                String kategori = rs.getString("kategori");
                 String harga_Jual = String.valueOf(rs.getInt("harga_jual"));
                 
-                String tbData[] = {id,nama_Produk,harga_Jual};
+                String tbData[] = {id,nama_Produk,kategori,harga_Jual};
                 
-                DefaultTableModel tblModel = (DefaultTableModel) jtable.getModel();
                 tblModel.addRow(tbData);
             }
+            
+        }catch(SQLException sqle){
+            System.out.println(sqle);
+        }
+    }
+    
+    public void Display_detailBelanja(JTable jtable){
+        
+        try{
+            DefaultTableModel tblModel = (DefaultTableModel) jtable.getModel();
+            tblModel.setRowCount(0);
+            Statement stmt = (Statement) kn.getKoneksi().createStatement();
+            String sql = "Select * FROM data_detailbelanja";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                String id = String.valueOf(rs.getInt("id_barang"));
+                String nama_Produk = rs.getString("nama_Barang");
+                String kategori = rs.getString("kategori");
+                String harga_Jual = String.valueOf(rs.getInt("harga_jual"));
+                
+                String tbData[] = {id,nama_Produk,kategori,harga_Jual};
+                
+                tblModel.addRow(tbData);
+            }
+            
+        }catch(SQLException sqle){
+            System.out.println(sqle);
+        }
+    }
+    
+    public void add_Keranjang(String id, String nama,String kategori, String hargaJ){
+        
+        try{
+            int idB = Integer.parseInt(id);
+            int hargaJual = Integer.parseInt(hargaJ);
+            Statement stmt = (Statement) kn.getKoneksi().createStatement();
+            String sql = "INSERT INTO data_detailbelanja VALUES ('"+idB+"','"+nama+"','"+kategori+"',"+hargaJual+")";
+            stmt.executeUpdate(sql);
             
         }catch(SQLException sqle){
             System.out.println(sqle);
